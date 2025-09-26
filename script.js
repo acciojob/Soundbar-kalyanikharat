3978814da0f41b09cbf30d2dadcd45800c9c5879
kalyanikharat <132137407+kalyanikharat@users.noreply.github.com>
2025-09-26 17:07:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const sounds = [
+  "applause.mp3",
+  "boo.mp3",
+  "gasp.mp3",
+  "tada.mp3",
+  "victory.mp3",
+  "wrong.mp3",
+  "stop.mp3",
+  
+];
+
+for (let i = 1; i <= 9; i++) {
+  const btn = document.getElementById(`btn${i}`);
+  btn.addEventListener("click", () => {
+    const audio = new Audio(`sounds/${sounds[i - 1]}`);
+    audio.play();
+  });
+}
